92fc64dfe01f7dfa09f3ee22bc38f18faaede486
SWETA2318 <150820032+SWETA2318@users.noreply.github.com>
2023-12-29 17:57:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
+// Global object
 const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
+// Function to check if key exists in the global object
 function hasKey(key) {
-  //   write your code here
+  // Check if the key exists in the sampleObject
+  return key in sampleObject;
 }
 
-// Do not change the code below
+// Prompt for user input
 const key = prompt("Enter Key.");
+
+// Display result using alert
 alert(hasKey(key));
